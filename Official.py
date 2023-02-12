ae8448e1cede6f788f543985f5bbe0bf07cfbb8c
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-02-12 19:18:00 +0530

    Add files via upload

---
+++ Official.py
@@ -0,0 +1,36 @@
+
+from selenium import webdriver
+from time import sleep
+import time
+
+
+driver = webdriver.Chrome()
+
+driver.get('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?response_type=id_token&scope=openid%20profile&client_id=5e3ce6c0-2b1f-4285-8d4b-75ee78787346&redirect_uri=https%3A%2F%2Fteams.microsoft.com%2Fgo&state=eyJpZCI6IjE2ODExYTFlLTAxYzAtNGYyOS1hYjc4LWYzOTRlZmUwOTQ2NiIsInRzIjoxNjMzNTE2OTMwLCJtZXRob2QiOiJyZWRpcmVjdEludGVyYWN0aW9uIn0%3D&nonce=74ed1ad1-6f2b-4f78-a4a4-50f78f3b8d8b&client_info=1&x-client-SKU=MSAL.JS&x-client-Ver=1.3.4&client-request-id=d738128f-18ce-42e4-b29c-d6d4e3dcd75e&response_mode=fragment')
+
+time.sleep(2)
+
+driver.find_element_by_id("i0116").send_keys("vikash.chand2020@vitstudent.ac.in")
+time.sleep(2)
+driver.find_element_by_id("idSIButton9").click()
+print('Username Entered')
+time.sleep(5)
+
+
+
+
+
+
+
+driver.find_element_by_id("i0118").send_keys("")
+time.sleep(2)
+driver.find_element_by_id("idSIButton9").click()
+print('Password Entered')
+
+driver.find_element_by_id("idSIButton9").click()#yes button always remember
+print('Yes Button Has Been Clicked')
+time.sleep(3)
+
+driver.find_element_by_class_name("table-row").click()
+
+
---
+++ README.md
@@ -0,0 +1,62 @@
+
+# Project Title
+INSTAGRAM AUTOMATION USING SELENIUM 
+
+SELENIUM EMBEDED WITH PYTHON 
+
+Automated the microsoft teams webpage .
+
+login with credentials
+
+send text messages
+
+
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+PYTHON
+
+selenium webdriver
+
+chrome webdriver extension
+
+VSCODE OR ANY OTHER TERMINAL TO RUN CODE
+
+
+
+
+
+
+## STEPS
+# spam-bots
+ensure python in pc
+
+download the file
+
+unzip it
+ 
+install the selenium and chrome driver and give the location of chrome driver in path 
+
+open the folder with vs code
+
+Enter the credentials  
+
+run the code
+
+
+🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+## Demo
+https://www.linkedin.com/posts/vikash-chand28_testing-microsoftteams-python-activity-6851641121106341888-O-T4
+## Feedback
+
+If you have any feedback, please reach out to us at VIKASHCHAND147@GMAIL.com
+
+
+## Authors
+
+- [@vikashchand](https://github.com/vikashchand)
+
